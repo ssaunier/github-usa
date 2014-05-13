300ebc84c8348e39075d888cd59f6ffc044b4c7b
Sebastien Saunier <seb@saunier.me>
2014-05-13 15:14:11 +0200

    Opacity on logarithm scale

--- map.rb
+++ map.rb
@@ -1,8 +1,10 @@
 # Usage: ruby -Ilib map.rb
 
 require 'geo'
+
 require 'csv'
 require 'json'
+require 'colorize'
 
 max_ratio = nil
 
@@ -11,12 +13,16 @@ features = []
 csv = CSV.parse(File.read('data/github_users_per_state.csv'), :headers => true)
 csv.each do |row|
   row_hash = row.to_hash
+  abbr = row_hash["Abbr"]
+  puts "Fetching #{abbr}"
   ratio = row["Dev per 1000 inhab."].to_f
   max_ratio = ratio if max_ratio.nil?
+  opacity = Math.log(Math::E * ratio / max_ratio)
   begin
-    features << Geo.new.geo_json(row["Abbr"], row_hash, ratio / max_ratio)
+    features << Geo.new.geo_json(abbr, row_hash, opacity)
   rescue OpenURI::HTTPError => e
-    puts "Could not fetch geojson for #{row_hash["Abbr"]} - #{e.message}"
+    puts "Could not fetch geojson for #{row_hash["Abbr"]} - #{e.message}".colorize(:yellow)
+    retry unless e.message =~ /404/
   end
 end
 
